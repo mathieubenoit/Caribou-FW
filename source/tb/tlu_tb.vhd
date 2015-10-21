3ca7941f2b8bff0cf466a18d25d17cbc027b4612
hongbin <hongbin.liu@cern.ch>
2015-10-21 16:13:33 -0400

    add tlu master modules into the project

---
+++ source/tb/tlu_tb.vhd
@@ -0,0 +1,126 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 10/21/2015 02:26:47 PM
+-- Design Name: 
+-- Module Name: tlu_tb - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity tlu_tb is
+end tlu_tb;
+
+architecture Behavioral of tlu_tb is
+
+signal sysclk               :std_logic;
+signal clk160m              :std_logic;
+signal clk40m               :std_logic;
+
+signal rst                  :std_logic;
+signal tlu_master_output_en :std_logic;
+
+signal tlu_tri_clk          :std_logic;
+signal tlu_busy             :std_logic;
+signal tlu_reset            :std_logic;
+signal tlu_trigger          :std_logic;
+
+signal fei4_cmd_out         :std_logic;
+
+begin
+
+clk160_gen:process
+   begin
+        clk160m <= '0';
+        wait for 3.125 ns;  
+        clk160m <= '1';
+        wait for 3.125 ns;  
+end process;
+
+clk100_gen:process
+   begin
+        sysclk <= '0';
+        wait for 5 ns;  
+        sysclk <= '1';
+        wait for 5 ns;  
+end process;
+
+clk40_gen:process
+   begin
+        clk40m <= '0';
+        wait for 12.5 ns;  
+        clk40m <= '1';
+        wait for 12.5 ns;  
+end process;
+
+
+rst <= '0', '1' after 50 ns, '0' after 100 ns;
+tlu_master_output_en <= '0', '1' after 50 us;
+
+tlu_master:entity work.tlu_master_core
+Generic map(
+    sys_clk_freq => 100_000_000, --input clock speed from user logic in Hz
+    trig_rate    => 500_000
+) 
+Port map(
+    RST           => rst,
+    SYSCLK        => sysclk,
+    
+    OUT_EN        => tlu_master_output_en,
+    
+    -- TLU signals
+    TRIGGER_CLK_I => tlu_tri_clk,
+    BUSY_I        => tlu_busy,
+    RESET_O       => tlu_reset,
+    TRIGGER_O     => tlu_trigger
+);
+
+fei4_a1_cfg:entity work.FEI4B_CFG
+Port map(     
+    RST             => rst,
+    
+    CLK160          => clk160m,
+    CMD_OUT_PH_SEL  =>   "00",
+    
+    CMD_CLK         => clk40m,
+    FR_CFG_CLK      => sysclk,
+    FR_RAM_ADDR     => (others => '0'),
+    FR_RAM_DAT_IN   => (others => '0'),
+    FR_RAM_DAT_OUT  => open,
+    
+    CFG_FLG         => '0',    
+    CFG_REG         => (others => '0'),
+    WR_REG_DAT      => (others => '0'),
+                
+    CMD_OUT         => fei4_cmd_out,
+    
+    EXT_TRI         => tlu_trigger,
+    BUSY            => tlu_busy  
+);
+
+
+
+end Behavioral;
---
+++ source/tlu/tlu_master.vhd
@@ -0,0 +1,132 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 10/21/2015 02:13:43 PM
+-- Design Name: 
+-- Module Name: tlu_master - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity tlu_master_core is
+    Generic(
+    sys_clk_freq : INTEGER := 100_000_000; --input clock speed from user logic in Hz
+    trig_rate    : INTEGER := 100_000
+    );   
+    Port (
+    RST           :in std_logic;
+    SYSCLK        :in std_logic;
+    
+    OUT_EN        :in std_logic;
+    
+    -- TLU signals
+    TRIGGER_CLK_I :in std_logic;
+    BUSY_I        :in std_logic;
+    RESET_O       :out std_logic;
+    TRIGGER_O     :out std_logic
+    );
+end tlu_master_core;
+
+architecture Behavioral of tlu_master_core is
+
+constant divider    :  integer := (sys_clk_freq/trig_rate)/2;
+
+signal tri_clk      :std_logic;
+signal tri_clk_prev :std_logic;
+
+signal trigger_i    :std_logic;
+signal busy_pre     :std_logic;
+
+signal wait_cnt     :integer:= 0;
+
+TYPE machine IS(idle, trigger); --needed states
+signal state :machine;
+
+begin
+
+tri_clk_gen:process(SYSCLK, RST)
+  variable count  :  integer range 0 to divider*2;  --timing for clock generation
+begin
+  if(RST = '1') then                --reset asserted
+    count := 0;
+    tri_clk <= '0';   
+  elsif rising_edge(SYSCLK) then
+    tri_clk_prev <= tri_clk;            --store previous value of data clock
+    if(count = divider*2-1) then        --end of timing cycle
+      count := 0;                       --reset timer
+    else                                
+      count := count + 1;               --continue clock generation timing
+    end if;
+    
+    case count is
+      when 0 TO divider-1 =>            --first 1/2 cycle of clocking
+        tri_clk <= '0';
+      when divider TO divider*2-1 =>    --second 1/2 cycle of clocking
+        tri_clk <= '1';       
+      when others =>                    
+        null;
+    end case;
+  end if;
+end process;
+
+trigger_fsm:process(SYSCLK, RST)
+begin
+if RST = '1' then
+  trigger_i <= '0';
+  wait_cnt <= 0;
+  busy_pre <= '0';
+elsif rising_edge(SYSCLK) then
+
+  busy_pre <= BUSY_I;
+  case state is
+   
+  when idle =>
+    wait_cnt <= 0;
+    trigger_i <= '0';
+    if tri_clk = '1' and tri_clk_prev = '0' and OUT_EN = '1' then
+      state <= trigger;
+      trigger_i <= '1';
+    end if;
+    
+  when trigger =>
+    if BUSY_I = '1' and busy_pre = '0' then
+      state <= idle;
+      wait_cnt <= 0;
+    elsif wait_cnt = divider then 
+      state <= idle;     
+    else
+      wait_cnt <= wait_cnt + 1;
+    end if;   
+  end case;
+  
+end if;
+end process;
+
+TRIGGER_O <= trigger_i;
+    
+end Behavioral;
