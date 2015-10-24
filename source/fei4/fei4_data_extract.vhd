be34e6ac45886ea0b4d1e6347a727af3bcb7a592
hongbin <hongbin.liu@cern.ch>
2015-10-23 22:40:19 -0400

    add comments

--- source/fei4/fei4_data_extract.vhd
+++ source/fei4/fei4_data_extract.vhd
@@ -1,16 +1,16 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Company: BNL
+-- Engineer: Hongbin Liu hliu2@bnl.gov
 -- 
 -- Create Date: 08/17/2015 03:36:34 PM
 -- Design Name: 
 -- Module Name: fei4_data_extract - Behavioral
--- Project Name: 
--- Target Devices: 
+-- Project Name: CaRIBOU Firmware
+-- Target Devices: None
 -- Tool Versions: 
 -- Description: 
 -- 
--- Dependencies: 
+-- Dependencies: None
 -- 
 -- Revision:
 -- Revision 0.01 - File Created
@@ -35,10 +35,10 @@ entity fei4_data_extract is
     Port ( 
     RESET           : in STD_LOGIC;  
       
-    FRAME_CLK       : in std_logic;  
-    FE_FR_RD_EN     : in std_logic; 
-    FRAME_IS_SYNCED : in std_logic;   
-    DATA_IN         : in std_logic_vector(7 downto 0);
+    FRAME_CLK       : in std_logic;  -- Frame clock, 16 MHz
+    FE_FR_RD_EN     : in std_logic;  -- Read Front end shift register indicator
+    FRAME_IS_SYNCED : in std_logic;  -- Frame is synced indicator 
+    DATA_IN         : in std_logic_vector(7 downto 0); -- Deserilized data input
     
     IS_PIX_DAT      : out std_logic; -- indicate current data is pixel/SR data 
     -- Bit[15] of REG_ADDR_OUT is the address type 
@@ -53,13 +53,13 @@ signal fclk           :std_logic;
 signal reg_value      :std_logic_vector(15 downto 0);
 signal reg_addr       :std_logic_vector(15 downto 0);
 
-signal ar_cnt         :integer range 0 to 1 := 0;
-signal vr_cnt         :integer range 0 to 1 := 0;
+signal ar_cnt         :integer range 0 to 1 := 0; -- address record counter
+signal vr_cnt         :integer range 0 to 1 := 0; -- value recorde counter
 
 type   state_type is (START, IDLE, SOF, EOF, AR, VR, DAT, FINISH, ERROR);
 signal state          : state_type := START ;
 
-signal is_pixel_data  :std_logic;  
+signal is_pixel_data  :std_logic;  -- current data is pixel data
 
 --attribute MARK_DEBUG : string;
 --attribute MARK_DEBUG of reg_value,reg_addr : signal is "TRUE";
@@ -68,6 +68,7 @@ begin
 
 fclk <= FRAME_CLK;
 
+-- Data extraction finite state machine 
 data_ext_fsm:process(RESET, fclk) 
 begin
 
@@ -84,43 +85,46 @@ elsif rising_edge(fclk) then
     
      when START =>
        if FRAME_IS_SYNCED = '1' then
-         state <= IDLE;
+         state     <= IDLE;
          reg_value <= (others => '0');
-         reg_addr <= (others => '0');     
-         ar_cnt <= 0;
-         vr_cnt <= 0;          
+         reg_addr  <= (others => '0');     
+         ar_cnt    <= 0;
+         vr_cnt    <= 0;          
        end if;
-     
-     when IDLE =>
-       if FRAME_IS_SYNCED = '0' then
+
+    
+     when IDLE =>   
+       if FRAME_IS_SYNCED = '0' then -- lost synchronization, move to start state
          state <= START;
-       elsif DATA_IN = x"FC" then
+       elsif DATA_IN = x"FC" then    -- x"FC" indicates the start of an frame
          state <= SOF;
        end if;
      
      when SOF =>
-       if DATA_IN = x"EA" then
+      
+       if DATA_IN = x"EA" then       -- x"EA" is the start of the address record
          if FE_FR_RD_EN = '1' then
            -- Write the contents from front end shift register to FIFO
            state <= DAT;
            is_pixel_data <= '1';
          else
            state <= AR;
-         end if;
-       elsif DATA_IN = x"EC" then
-         state <= VR;     
-       elsif DATA_IN = x"E9" then
+         end if;     
+       elsif DATA_IN = x"EC" then    -- x"EC" is the start of the value record
+         state <= VR;
+      
+       elsif DATA_IN = x"E9" then    -- x"E9" is the start of the pixle data header
          state <= DAT;
          is_pixel_data <= '1';
        end if;
      
      when AR =>
-       if DATA_IN = x"EC" then          
-         state <= VR;
+       if DATA_IN = x"EC" then       -- x"EC" is the start of the value record
+         state  <= VR;
          ar_cnt <= 0;
-       elsif ar_cnt = 1 then
-         reg_addr(7 downto 0) <= DATA_IN;
-       else
+       elsif ar_cnt = 1 then         -- ar_cnt = 1, read 8 LSB of the reg address
+         reg_addr(7 downto 0)  <= DATA_IN;
+       else                          -- ar_cnt = 0, read 8 MSB of the reg address
          reg_addr(15 downto 8) <= DATA_IN;
          ar_cnt <= ar_cnt + 1;
        end if;
@@ -129,20 +133,20 @@ elsif rising_edge(fclk) then
        if DATA_IN = x"BC" then          
          state <= EOF;
          vr_cnt <= 0;
-       elsif vr_cnt = 1 then
+       elsif vr_cnt = 1 then         -- vr_cnt = 1, read 8 LSB of the reg value 
          reg_value(7 downto 0) <= DATA_IN;
-       else
-         reg_value(15 downto 8) <= DATA_IN;
+       else                          -- vr_cnt = 0, read 8 MSB of the reg value
+         reg_value(15 downto 8)<= DATA_IN;
          vr_cnt <= vr_cnt + 1;
        end if;
          
      when DAT =>
-       if DATA_IN = x"BC" then          
+       if DATA_IN = x"BC" then       -- de-assert the pixle data flg at EOF                   
          state <= EOF;
          is_pixel_data <= '0';   
        end if;
 
-     when EOF =>
+     when EOF =>                     -- if next frame is following, move to SOF
        if DATA_IN = x"FC" then
          state <= SOF;
        else
@@ -151,8 +155,8 @@ elsif rising_edge(fclk) then
      
      when FINISH =>
        if FRAME_IS_SYNCED = '0' then
-       state <= START;
-     end if;
+         state <= START;
+       end if;
        
      when others =>
           null;
